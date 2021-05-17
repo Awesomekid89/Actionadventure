26c2281fee8dcaa243bd789ea8b5ef052d8a0163
Awesomekid89 <73956641+Awesomekid89@users.noreply.github.com>
2021-05-17 11:32:15 -0700

    Create ActionAdventure.py

---
+++ ActionAdventure.py
@@ -0,0 +1,121 @@
+print('#################')
+print('Chemical Kids')
+print('#################')
+
+
+print('Prolouge')
+
+
+
+print('Hurry up! We cannot get caught!')
+print('I cannot keep up much longer, my legs are getting tired!')
+print('I understand, but do you want to go back to the labs?!')
+print('No!')
+print('Then hurry up!')
+print('I cannot make it much farther!')
+print('I cannot lose you Mark!')
+print('I understand, but I will only slow you down. Let me sacrifice myself so you can get to safety.')
+print('Mark! Do not every speak to me like that again! I will not let you sacrfice yourself!')
+print('It is the best option for me and you right now. I am only going to slow you down because I am overweight and I cannot run for long periods of time.')
+print('Sarah it is ok. I will spare you some time! Go!')
+# Sarah starts tearing up.
+print('Bye Sarah, it was great knowing you.')
+# Mark steps into the cloud of gas and is pulled back by a bunch of guys in radiation suits.
+print('Mark! No!')
+# Sarah runs away from the gas and the men and starts crying while she is running.
+
+
+print('############')
+print('Part 1')
+print('############')
+
+
+
+# Sarah is talking with a therapist.
+print('I was 13-years old at the time and Mark was 17-years old.')
+print('I see. Might I ask what lead up to this?')
+print('In the beginning, me and Mark were best friends. Both of our parents knew each other very well, and we both lived side-by-side.')
+print('I mean what happened in the beginning that lead up to you trying to escape a lab.')
+print('Oh. Me and Mark were walking down the street and we originally saw a white van come down our way but we thought nothing of it. 
+Soon enough, the van was approaching and did not have a clue on what was going to happen.')
+print('The white van then stopped near us and a bunch of men in black clothing came out and grabbed us. Mark had a knife and stabbed one in the heart. 
+They subdued him right away.')
+print('Then one of the men in the black clothing grabbed me. At the time I was defenseless and did not know how to defend myself.')
+print('Why did the men in the black clothing grab you guys?')
+print('Me and Mark were never sure of that point.')
+print('There had to be a reason. I mean, this is not your fault, just letting you know.')
+print('I know.')
+print('I want to traverse deeper into what happened , is that alright with you?')
+print('Yes. I want to know more about what happened to me and Mark.')
+print('Ok, then what happened?')
+print('Then we and Mark were subdued, they put blindfolds over our eyes and had restraints on our legs and hands.')
+print('I am very sorry for what happend to you Sarah.')
+print('You should not be, we were not aware of our surroundings.')
+print('Let us get back into what happened.')
+print('Then, the white van stopped and the men in the black clothing took us out of the van and mind you we still had our blindfolds on and we could not see anything, yet.')
+print('Ok. I am just going to be here listening and writing down some notes, continue on.')
+print('Me and Mark came to a abrupt stop and then the men in the black clothing took our blindfolds off and we were looking at a man about the age of 89.')
+print('That is odd. They kidnap you and bring you to a man that is about to die probably soon.')
+print('Yeah. But the man was in a lab coat and he was staring at some screen, but the screen was a hologram.')
+print('That is weird. Did they mention where you were at, or did you see some surrounding features of where you were at?')
+print('I did see one thing through the blindfold, a key card door.')
+print('So, it seems this is a heavily guarded place or warehouse, wherever they were holding you and Mark.')
+print('It was not just me and Mark there at the mysterious place, there were other people in what looked like cryo pods.')
+print('So, like Fallout 4 cryo pods or different?')
+print('Just like Fallout 4 cryo pods.')
+# For people who have not played Fallout 4, in the beginning, a nuke drops and you are evacuated to a bunker or vault. Then in the game, you are in a cryo pod.
+print('Yeah, it was weird. The weirdest part was that the humans in the cryo pods were almost like abominations.')
+print('What kind of creatures of abominations?')
+print('If I remember, some of the things I saw in the cryo pods were what looked life were wolfs, yetis, etc.')
+print('Strange. If this company or small group of people are conducting these kids of research on people then we have to bring this to the police and see what they can do.')
+print('Yeah I guess so. I would like to see them go down.')
+print('What you have told me, is all of this true? I just want to make sure because, the police might want more information.')
+print('Yes it is true and I have plenty of more information, actually tons of it.')
+print('Let me go make a quick call, I will be right back.')
+# Sarah waits until the therapist comes back.
+# The therapist comes back.
+print('Ok, I got some news. The police are coming and they are sending a few of their people to analyze your information.')
+print('Did you make sure they were actually police? I mean with bizare as the story is I do not think police would believe it.')
+print('Let me check that, hold on.')
+# The therapist goes to her manager and ask him to verify if the police are real.
+# Sarah starts thinking.
+print('There is no way in hell that the people who kidnapped me can impersonate a police officer!')
+# The thearpist comes back.
+print('I just spoke with manager. It turns out he is one the cheifs in command at the nearest police station. They said they are coming.')
+print('Ok good. I cannot let my guard down though.')
+print('I understand that totally. After what you have been through, I would totally understand.')
+print('Thanks.')
+print('You are welcome.')
+# Sarah and the therapist talk for a good bit while they wait for the police to show up.
+print('What else happened? I would like to know more for the police.')
+print('Well, after me and Mark got to see what was going on, they instantly had the men in the black clothing take us to a surgery room.')
+print('When we were going into the surgery room I saw a door that did not have to have a key card to open. I keep note of that door.')
+print('On what side was that door on?')
+print('Coming into the surgery room it looked like it was on the left side. There also was a window in the door. I think that is a scouting door.')
+print('That could be a possibility. But we can never be unsure.')
+print('When I passed the door, I saw a bunch of other rooms. They were liked dentist rooms. They had the dentist chairs and the room space had the dentist spacey feeling.')
+print('I see. That is odd. I would guess that the people who are kidnapping people are making sure they do not see what is happening.')
+print('Maybe they have multiple stations or warehouses. I was just able to see just a little and then they put the blindfold back on me and restrained me again.')
+print('I see. Also odd and strange. When you were in the dentist chair, did they sit you upright?')
+print('Yes. How would you know?')
+print('I have been there myself. I was a doctor there, but when I found out what they were doing I asked for a medical leave and they were dumb enough to give it to me.')
+print('The head person their is part of an illegal science project. The science company is from the Dark Web.')
+print('It seems you know more than me.')
+print('Yeah, I had to know the history behind the company and all the rules, agreements, etc. It was a tiring job to do.')
+print('What lead you to do that job?')
+print('At the time, the job I had was not paying me enough money. Someone bumped into me and said NexaCorp. I did not know what they meant.')
+print('That is what the company is called?')
+print('Yes. The company is very top secret and not many people know about it.')
+print('I am suprised the FBI has not cracked down on it. Is there a reason for that?')
+print('Yes. Nexacorp has the best hackers, or as they say. There VPNs are constantly bouncing everywhere.')
+print('The cops are here.')
+print('Ok Sarah, it is time.')
+
+
+print('##########')
+print('Part 2')
+print('##########')
+
+
+
+print('
